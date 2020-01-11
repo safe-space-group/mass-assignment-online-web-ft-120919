21ee410e0173ee2c47f55345b41359c11de2aac1
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-11 03:36:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday. :hair_color, :eye_color, :height, :weight, :handed 
+                :wrist_size, :glove_size, :pant_length, :pant_width
+                
+  def initialiZe(name, birthday, hair_color, ete_color, height, weight, handed
+                 wrist_size, glove_size, pant_length, pant_width)
+    
 end
